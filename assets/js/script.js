c2fdb943634c31be7ff3575d80c6ec33ff3ba15b
Felipe <felipe.sarmiento.as@gmail.com>
2023-06-20 08:34:01 -0400

    create array of objects to store questions and multiple choice

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+// create an array of object where I store all the static text - make it easier in the future to add more questions
+var questions = [{
+        questionText: "how many players from one team are allow to play inside the field?",
+        a: "22",
+        b: "18",
+        c: "11",
+        d: "9"
+    },
+    {
+        questionText: "how many players from one team are allow to play inside the field?",
+        a: "22",
+        b: "18",
+        c: "11",
+        d: "9"
+    },
+    {
+        questionText: "how many players from one team are allow to play inside the field?",
+        a: "22",
+        b: "18",
+        c: "11",
+        d: "9"
+    },
+    {
+        questionText: "how many players from one team are allow to play inside the field?",
+        a: "22",
+        b: "18",
+        c: "11",
+        d: "9"
+    },
+    {
+        questionText: "how many players from one team are allow to play inside the field?",
+        a: "22",
+        b: "18",
+        c: "11",
+        d: "9"
+    }
+]
+
+
+console.log (questions);
